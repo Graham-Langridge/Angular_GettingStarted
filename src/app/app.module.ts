9f163954e2df779fa951d521084de0f36e64f397
Graham Langridge <grahamlangridge@lineone.net>
2018-04-15 21:05:01 +0100

    Added product list.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './Products/product-list.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
   bootstrap: [AppComponent],
   providers: []
 })
