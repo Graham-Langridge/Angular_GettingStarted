7e0737c9863c6efcf5f3da5e60afe5599ea04ff3
Graham Langridge <grahamlangridge@lineone.net>
2018-12-13 11:30:40 +0000

    Routes for Product Details

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from './product';
 // Component created with the following command...
 // ng g c products/product-detail --flat
@@ -9,10 +10,28 @@ import { IProduct } from './product';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-
-  constructor() { }
+  pageTitle = 'Product Detail';
+  product: IProduct;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': id,
+      'productName': 'Video Game Controller',
+      'productCode': 'GMG-0042',
+      'releaseDate': 'October 15, 2015',
+      'description': 'Standard two-button video game controller',
+      'price': 35.95,
+      'starRating': 4.6,
+      'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png'
+    };
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
